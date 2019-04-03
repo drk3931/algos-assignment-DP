bc8fa34ac4683b2f99a6491d5fb46414f595c95c
Deepak Khemraj <drk3931@gmail.com>
2019-04-02 23:45:04 -0400

    added glassfalling.java,rodcutting.java and relevant reports

--- GlassFalling.java
+++ GlassFalling.java
@@ -5,8 +5,20 @@ public class GlassFalling {
 
   // Do not change the parameters!
   public int glassFallingRecur(int floors, int sheets) {
-    // Fill in here and change the return
-    return 0;
+
+    if (floors <= 1 || sheets == 1) {
+      return floors;
+    } else {
+      int minFalls = Integer.MAX_VALUE, minComparison;
+      for (int i = 1; i <= floors; i++) {
+        minComparison = Math.max(glassFallingRecur(i - 1, sheets - 1), glassFallingRecur(floors - i, sheets));
+        if (minComparison < minFalls) {
+          minFalls = minComparison;
+        }
+      }
+      return minFalls + 1;
+    }
+
   }
 
   // Optional:
@@ -17,22 +29,49 @@ public class GlassFalling {
   }
 
   // Do not change the parameters!
+
+  // Do not change the parameters!
   public int glassFallingBottomUp(int floors, int sheets) {
-    // Fill in here and change the return
-    return 0;
+
+    // the table represents the solutions to glassFalling(floor,sheets)
+    int glassFalling[][] = new int[sheets + 1][floors + 1];
+
+    // initialize the second row to 0 through n
+    for (int i = 0; i <= floors; i++) {
+      glassFalling[1][i] = i;
+    }
+
+    for (int i = 2; i <= sheets; i++) {
+      for (int j = 1; j <= floors; j++) {
+
+        // at most it could be the highest floor, work to find the smallest floor
+        int minimum = floors;
+
+        for (int z = 1; z <= j; z++) { // one attempt + the fewest possible number of attempts
+          // which is yielded by glassFalling(i,j-z) and glassFalling(i-1,j-1) as per
+          // recursive formulation
+          minimum = Math.min(minimum, (1 + Math.max(glassFalling[i][j - z], glassFalling[i - 1][z - 1])));
+        }
+
+        // set best and continue upwards
+        glassFalling[i][j] = minimum;
+      }
+    }
+
+    return glassFalling[sheets][floors];
+
   }
 
+  public static void main(String args[]) {
+    GlassFalling gf = new GlassFalling();
 
-  public static void main(String args[]){
-      GlassFalling gf = new GlassFalling();
-
-      // Do not touch the below lines of code, and make sure
-      // in your final turned-in copy, these are the only things printed
-      int minTrials1Recur = gf.glassFallingRecur(27, 2);
-      int minTrials1Bottom = gf.glassFallingBottomUp(27, 2);
-      int minTrials2Recur = gf.glassFallingRecur(100, 3);
-      int minTrials2Bottom = gf.glassFallingBottomUp(100, 3);
-      System.out.println(minTrials1Recur + " " + minTrials1Bottom);
-      System.out.println(minTrials2Recur + " " + minTrials2Bottom);
+    // Do not touch the below lines of code, and make sure
+    // in your final turned-in copy, these are the only things printed
+    int minTrials1Recur = gf.glassFallingRecur(27, 2);
+    int minTrials1Bottom = gf.glassFallingBottomUp(27, 2);
+    int minTrials2Recur = gf.glassFallingRecur(100, 3);
+    int minTrials2Bottom = gf.glassFallingBottomUp(100, 3);
+    System.out.println(minTrials1Recur + " " + minTrials1Bottom);
+    System.out.println(minTrials2Recur + " " + minTrials2Bottom);
   }
 }
--- RodCutting.java
+++ RodCutting.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Rod cutting problem described in Chapter 15 of textbook
  */
@@ -5,29 +7,65 @@ public class RodCutting {
 
   // Do not change the parameters!
   public int rodCuttingRecur(int rodLength, int[] lengthPrices) {
-    return 0;
+
+    // the problem cannot be computed on negative inputs, smallest case yields 0
+    if (rodLength <= 0) {
+      return 0;
+    }
+
+    // compare all other lengths against first index.
+    int maxCuts = lengthPrices[0];
+
+    for (int i = 0; i < rodLength; i++) {
+
+      // expands to max(maxCuts, (max (maxCuts (max (maxCuts (.... 0))))))
+      maxCuts = Math.max(maxCuts, lengthPrices[i] + rodCuttingRecur(rodLength - i - 1, lengthPrices));
+
+    }
+
+    // return once resolved
+    return maxCuts;
+
   }
 
   // Do not change the parameters!
   public int rodCuttingBottomUp(int rodLength, int[] lengthPrices) {
-    return 0;
+
+    int savedMaxCuts[] = new int[rodLength + 1];
+
+    // equivalent to 0 base case in recursive formulation
+    savedMaxCuts[0] = 0;
+
+    // solving upwards until rodLength, start at one as base case already solved.
+    for (int i = 1; i <= rodLength; i++) {
+
+      int maxCuts = lengthPrices[0];
+      for (int z = 0; z < i; z++) {
+        // lookup saved
+        maxCuts = Math.max(maxCuts, lengthPrices[z] + savedMaxCuts[i - z - 1]);
+        // save result after resolved
+        savedMaxCuts[i] = maxCuts;
+
+      }
+    }
+    // return once solved up to this point
+    return savedMaxCuts[rodLength];
   }
 
+  public static void main(String args[]) {
+    RodCutting rc = new RodCutting();
 
-  public static void main(String args[]){
-      RodCutting rc = new RodCutting();
-
-      // In your turned in copy, do not touch the below lines of code.
-      // Make sure below is your only output.
-      int length1 = 7;
-      int[] prices1 = {1, 4, 7, 3, 19, 5, 12};
-      int length2 = 14;
-      int[] prices2 = {2, 5, 1, 6, 11, 15, 17, 12, 13, 9, 10, 22, 18, 26};
-      int maxSell1Recur = rc.rodCuttingRecur(length1, prices1);
-      int maxSell1Bottom = rc.rodCuttingBottomUp(length1, prices1);
-      int maxSell2Recur = rc.rodCuttingRecur(length2, prices2);
-      int maxSell2Bottom = rc.rodCuttingBottomUp(length2, prices2);
-      System.out.println(maxSell1Recur + " " + maxSell1Bottom);
-      System.out.println(maxSell2Recur + " " + maxSell2Bottom);
+    // In your turned in copy, do not touch the below lines of code.
+    // Make sure below is your only output.
+    int length1 = 7;
+    int[] prices1 = { 1, 4, 7, 3, 19, 5, 12 };
+    int length2 = 14;
+    int[] prices2 = { 2, 5, 1, 6, 11, 15, 17, 12, 13, 9, 10, 22, 18, 26 };
+    int maxSell1Recur = rc.rodCuttingRecur(length1, prices1);
+    int maxSell1Bottom = rc.rodCuttingBottomUp(length1, prices1);
+    int maxSell2Recur = rc.rodCuttingRecur(length2, prices2);
+    int maxSell2Bottom = rc.rodCuttingBottomUp(length2, prices2);
+    System.out.println(maxSell1Recur + " " + maxSell1Bottom);
+    System.out.println(maxSell2Recur + " " + maxSell2Bottom);
   }
 }
